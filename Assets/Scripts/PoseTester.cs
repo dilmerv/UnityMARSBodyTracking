1f12b1b1da7f10d894bfa2e46c0cad5f96d4cef2
Dilmer Valecillos <dilmervst@gmail.com>
2021-11-01 08:39:39 -0600

    Added changes for body tracking scene

---
+++ Assets/Scripts/PoseTester.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PoseTester : MonoBehaviour
+{
+    public void BodyAction(string actionType)
+    {
+        Debug.Log(actionType);
+        Logger.Instance.LogInfo(actionType);   
+    }
+}
